6a303f0e46e216763c9154fd28ab5bfdba2db3e7
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-03-04 15:18:01 +0100

    Aggiungere i file di progetto.

---
+++ MVC-CORE-EF-1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_CORE_EF_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
